add05297a39f996c15c3ede39a2bda9da1bc4ef6
CommandCube <63754664+CommandCube@users.noreply.github.com>
2022-07-15 11:34:47 -0400

    Added flight ability to ring, as well as seccond ring which is breakable. Updated textures

--- src/main/java/net/galacticsnail/angelrings/item/ModItems.java
+++ src/main/java/net/galacticsnail/angelrings/item/ModItems.java
@@ -1,20 +1,28 @@
 package net.galacticsnail.angelrings.item;
 
+import dev.emi.trinkets.api.TrinketItem;
+import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.galacticsnail.angelrings.AngelRings;
 import net.galacticsnail.angelrings.item.custom.AngelRing;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item ANGEL_RING = registerItem("angel_ring",
-            new AngelRing(new FabricItemSettings().group(ItemGroup.TOOLS)));
+            new AngelRing(new FabricItemSettings().group(ItemGroup.TOOLS), false));
 
+    public static final Item ANGEL_RING_BREAKABLE = registerItem("angel_ring_breakable",
+            new AngelRing(new FabricItemSettings().group(ItemGroup.TOOLS), true));
+//
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(AngelRings.MOD_ID, name), item);
     }
+    //TrinketItem(ANGEL_RING.Settings);
+
+    //TrinketsApi.registerTrinket(ANGEL_RING, new AngelRing());
 
     public static void registerModItems() {
         AngelRings.LOGGER.info("Registering Mod Items for " + AngelRings.MOD_ID);
--- src/main/java/net/galacticsnail/angelrings/item/custom/AngelRing.java
+++ src/main/java/net/galacticsnail/angelrings/item/custom/AngelRing.java
@@ -1,28 +1,81 @@
 package net.galacticsnail.angelrings.item.custom;
 
-import dev.emi.trinkets.api.SlotReference;
-import dev.emi.trinkets.api.Trinket;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
+import dev.emi.trinkets.api.*;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.item.Item;
 
-public class AngelRing extends Item {
-    public AngelRing(Settings settings) {
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class AngelRing extends TrinketItem {
+//    public AngelRing(Settings settings) {
+//        super(settings);
+//    }
+    public boolean isDurable;
+    public AngelRing(Settings settings, boolean isDurable) {
         super(settings);
+        this.isDurable = isDurable;
+        TrinketsApi.registerTrinket(this, this);
     }
 
+//    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+//        Multimap<EntityAttribute, EntityAttributeModifier> map = super.getModifiers(stack, slot, entity, uuid);
+//        // +10% movement speed
+//        map.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uuid, "angelrings:movement_speed", 0.1, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+//        // If the player has access to ring slots, this will give them an extra one
+//        SlotAttributes.addSlotModifier(map, "hand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
+//        return map;
+//    }
+
+
+    //@Override
+    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        PlayerEntity player = (PlayerEntity) entity;
+        //if the instance of the object has isDurable set to true, the item will eventually break
+        if (isDurable && player.getAbilities().flying) {
+            //will take additional durability off if player is sprinting
+            if (player.isSprinting()) {
+                //will damage ring 2% of time
+                if (Math.random() < 0.02) {
+                    stack.damage(2, player, e -> e.sendEquipmentBreakStatus(EquipmentSlot.LEGS));
+                }
+            }
+            //logic for when player is not sprint flying
+            else {
+                if (Math.random() < 0.02) {
+                    stack.damage(1, player, e -> e.sendEquipmentBreakStatus(EquipmentSlot.LEGS));
+                }
+            }
+
+        }
+    }
+
+    //@Override
     public void onEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
         PlayerEntity player = (PlayerEntity) entity;
         player.getAbilities().allowFlying = true;
     }
 
+    //@Override
     public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
         PlayerEntity player = (PlayerEntity) entity;
-        player.getAbilities().allowFlying = false;
-        player.getAbilities().flying = false;
-        player.sendAbilitiesUpdate();
+        //Will not disable flight if player in creative
+        if (!player.getAbilities().creativeMode && !player.isSpectator()) {
+            player.getAbilities().allowFlying = false;
+            player.getAbilities().flying = false;
+            player.sendAbilitiesUpdate();
+        }
     }
 
 }
